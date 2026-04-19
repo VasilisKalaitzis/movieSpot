b3e5a593f2474f3789167cbd32db90c3b4e3981d
VasilisKalaitzis <cs101092@gmail.com>
2026-04-19 23:48:28 +0200

    fix: broken url

--- src/static_data/mainContent.js
+++ src/static_data/mainContent.js
@@ -2,8 +2,8 @@
 // future modification, e.x to be retrieved from the server
 const mainContent = {
   current_view: "movieList",
-  data_url: "http://api.tvmaze.com/shows",
-  search_url: "http://api.tvmaze.com/search/shows"
+  data_url: "https://api.tvmaze.com/shows",
+  search_url: "https://api.tvmaze.com/search/shows"
 };
 
 export default mainContent;
